6471345f7ff46923f460539d721d735453ba2e92
Nuy <nurulazzahra.putri@my.jcu.edu.au>
2020-04-08 14:43:42 +0800

    initial commit

---
+++ src/components/footerComponent/footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { render } from "@testing-library/react";
+
+// import {  } from "../../../public/Piano_Website/Logo.png";
+class Footer extends Component {
+  render() {
+    return <footer></footer>;
+  }
+}
+
+export default Footer;
